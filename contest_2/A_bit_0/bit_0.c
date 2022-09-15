6b026c14f6026da5ce14fef4543ca6d46f7e3e62
cd7567 <alexander.y.koshelev@gmail.com>
2022-09-15 16:48:32 +0300

    2_A_bit_0 added

---
+++ contest_2/A_bit_0/bit_0.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned long long ull;
+
+char ToHEX(ull hex_digit) {
+  char digit;
+
+  if (hex_digit < 10) {
+    digit = hex_digit + 48;
+  } else {
+    digit = hex_digit + 87;
+  }
+
+  return digit;
+}
+
+int main() {
+  char lhs_hex[3], rhs_hex[3];
+  ull lhs, rhs, result = 0, exp = 1;
+
+  scanf("%s %s", lhs_hex, rhs_hex);
+
+  lhs = strtoll(lhs_hex, NULL, 16);
+  rhs = strtoll(rhs_hex, NULL, 16);
+  result = lhs ^ rhs;
+
+  return result;
+}
