b183e8777631bd1b7a9143c2ef0e05c38a2a5a12
cd7567 <alexander.y.koshelev@gmail.com>
2022-09-16 01:19:47 +0300

    1_J_rational fix

--- contest_1/J_rational/rational.c
+++ contest_1/J_rational/rational.c
@@ -2,27 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int toDec(char* roman) {
-  switch (*roman) {
-    case 'M':
-      return 1000;
-    case 'D':
-      return 500;
-    case 'C':
-      return 100;
-    case 'L':
-      return 50;
-    case 'X':
-      return 10;
-    case 'I':
-      return 1;
-  }
-}
-
 int main() {
   char roman[31];
   char* dot_ptr;
-  long long int_part;
   long double result;
 
   scanf("%s", roman);
