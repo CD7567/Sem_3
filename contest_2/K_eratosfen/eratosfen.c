8c81841b763d2e8e159c3f238d9d8b0dc0475622
cd7567 <alexander.y.koshelev@gmail.com>
2022-09-25 18:58:10 +0300

    2_K_eratosfen updated

--- contest_2/K_eratosfen/eratosfen.c
+++ contest_2/K_eratosfen/eratosfen.c
@@ -5,14 +5,15 @@
 typedef unsigned long long llu;
 
 int main() {
-  llu x, counter = 0;
+  llu x, x_half, counter = 0;
   unsigned char* buff;
 
   scanf("%llu", &x);
 
+  x_half = x / 2 + 1;
   buff = calloc(x / 8 + 1, sizeof(unsigned char));
 
-  for (llu i = 2; i <= x; ++i) {
+  for (llu i = 2; i <= x_half; ++i) {
     if ((buff[i / 8] >> (i % 8)) % 2 == 0) {
       for (llu j = i * i; j <= x; j += i) {
         buff[j / 8] += (((buff[j / 8] >> (j % 8)) % 2 == 1) ? 0 : 1 << j % 8);
