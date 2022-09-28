c7fbffe9b0a41dbbfddb3cd1adf9ee1b4ccd7a48
cd7567 <alexander.y.koshelev@gmail.com>
2022-09-28 14:03:39 +0300

    2_G_bit_5 fixed

--- contest_2/G_bit_5/bit_5.c
+++ contest_2/G_bit_5/bit_5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef unsigned long long ull;
 
@@ -16,83 +17,87 @@ char GetHexDigit(int decimal) {
 }
 
 int main() {
-  char characteristic[3];
-  char delimiter;
+  char characteristic[50] = {};
   int desc[6] = {-1, -1, -1, -1, -1, -1};
 
-  do {
-    scanf("%s", characteristic);
+  scanf("%[^\n]%*c", characteristic);
 
-    if (characteristic[0] == 'm' && characteristic[1] == 'a') {
+  for (size_t i = 0; i < 49; i += 2) {
+    if (characteristic[i] == 'm' && characteristic[i + 1] == 'a') {
       if (desc[0] == -1) {
         desc[0] = 1;
       }
-    } else if (characteristic[0] == 'f' && characteristic[1] == 'e') {
+    } else if (characteristic[i] == 'f' && characteristic[i + 1] == 'e') {
       if (desc[0] == -1) {
         desc[0] = 0;
       }
-    } else if (characteristic[0] == 'c' && characteristic[1] == 'l') {
+    } else if (characteristic[i] == 'c' && characteristic[i + 1] == 'l') {
       if (desc[1] == -1) {
         desc[1] = 1;
       }
-    } else if (characteristic[0] == 'd' && characteristic[1] == 'u') {
+    } else if (characteristic[i] == 'd' && characteristic[i + 1] == 'u') {
       if (desc[1] == -1) {
         desc[1] = 0;
       }
-    } else if (characteristic[0] == 'h' && characteristic[1] == 'a') {
+    } else if (characteristic[i] == 'h' && characteristic[i + 1] == 'a') {
       if (desc[2] == -1) {
         desc[2] = 1;
       }
-    } else if (characteristic[0] == 'n' && characteristic[1] == 'h') {
+    } else if (characteristic[i] == 'n' && characteristic[i + 1] == 'h') {
       if (desc[2] == -1) {
         desc[2] = 0;
       }
-    } else if (characteristic[0] == 't' && characteristic[1] == 'r') {
+    } else if (characteristic[i] == 't' && characteristic[i + 1] == 'r') {
       if (desc[3] == -1) {
         desc[3] = 1;
       }
-    } else if (characteristic[0] == 's' && characteristic[1] == 'k') {
+    } else if (characteristic[i] == 's' && characteristic[i + 1] == 'k') {
       if (desc[3] == -1) {
         desc[3] = 0;
       }
-    } else if (characteristic[0] == 'b' && characteristic[1] == 'n') {
+    } else if (characteristic[i] == 'b' && characteristic[i + 1] == 'n') {
       if (desc[4] == -1) {
         desc[4] = 0;
       }
-    } else if (characteristic[0] == 'r' && characteristic[1] == 'd') {
+    } else if (characteristic[i] == 'r' && characteristic[i + 1] == 'd') {
       if (desc[4] == -1) {
         desc[4] = 1;
       }
-    } else if (characteristic[0] == 'b' && characteristic[1] == 'w') {
+    } else if (characteristic[i] == 'b' && characteristic[i + 1] == 'w') {
       if (desc[4] == -1) {
         desc[4] = 2;
       }
-    } else if (characteristic[0] == 'b' && characteristic[1] == 'k') {
+    } else if (characteristic[i] == 'b' && characteristic[i + 1] == 'k') {
       if (desc[4] == -1) {
         desc[4] = 3;
       }
-    } else if (characteristic[0] == 'b' && characteristic[1] == 'u') {
+    } else if (characteristic[i] == 'b' && characteristic[i + 1] == 'u') {
       if (desc[5] == -1) {
         desc[5] = 0;
       }
-    } else if (characteristic[0] == 'g' && characteristic[1] == 'e') {
+    } else if (characteristic[i] == 'g' && characteristic[i + 1] == 'e') {
       if (desc[5] == -1) {
         desc[5] = 1;
       }
-    } else if (characteristic[0] == 'g' && characteristic[1] == 'y') {
+    } else if (characteristic[i] == 'g' && characteristic[i + 1] == 'y') {
       if (desc[5] == -1) {
         desc[5] = 2;
       }
-    } else if (characteristic[0] == 'd' && characteristic[1] == 'a') {
+    } else if (characteristic[i] == 'd' && characteristic[i + 1] == 'a') {
       if (desc[5] == -1) {
         desc[5] = 3;
       }
+    } else {
+      if (characteristic[i] != '\0') {
+        --i;
+      } else {
+        break;
+      }
     }
-
-    delimiter = getchar();
-  } while (delimiter != '\n' && delimiter != '\0');
+  }
 
   characteristic[0] = GetHexDigit(desc[4] % 2 + ((desc[4] >> 1) << 1) + ((desc[5] % 2) << 2) + ((desc[5] >> 1) << 3));
   characteristic[1] = GetHexDigit(desc[0] % 2 + (desc[1] << 1) + (desc[2] << 2) + (desc[3] << 3));
+  characteristic[2] = '\0';
   printf("%s", characteristic);
 }
