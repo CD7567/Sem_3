0d2083178240acca35dc2005459fc9ee83be7c87
cd7567 <alexander.y.koshelev@gmail.com>
2022-10-04 09:50:10 +0300

    3_D_flag added

--- contest_3/D_flag/flag.c
+++ contest_3/D_flag/flag.c
@@ -1,41 +1,73 @@
 #include <stdio.h>
-#include <stdint.h>
 
-const uint32_t thr_unit = 32;
-const uint32_t reg_unit = 2;
-const uint32_t mem_unit = 128;
-
-const uint32_t thr_av = 1536;
-const uint32_t reg_av = 32768;
-const uint32_t mem_av = 49152;
-
-uint32_t min(uint32_t a, uint32_t b, uint32_t c) {
-  a = (a < b) ? a : b;
-  return (a < c) ? a : c;
-}
+#ifndef REGISTERS
+#define REGISTERS 4
+#endif
 
 int main() {
-  FILE* in;
-  FILE* out;
-  uint32_t thr, reg, mem;
-  uint32_t act_thr, act_reg, act_mem = 1;
-  uint32_t thr_limit, reg_limit, mem_limit;
+  unsigned char regs[4] = {};
+  int com, lhs, rhs, flag = 0;
 
-  in = fopen("cuda.in", "r");
-  fscanf(in, "%d%d%d", &thr, &reg, &mem);
-  fclose(in);
+  scanf("%d", &com);
 
-  act_thr = thr + (thr_unit - thr % thr_unit) % thr_unit;
-  act_reg = reg + (reg_unit - reg % reg_unit) % reg_unit;
-  if (mem != 0) {
-    act_mem = mem + (mem_unit - mem % mem_unit) % mem_unit;
+  while (com != 0) {
+    if (com >> 7 & 1 && !flag) {
+      scanf("%*d%*d");
+      scanf("\n%d", &com);
+      continue;
+    }
+
+    com &= (1 << 7) - 1;
+
+    switch (com) {
+      case 1:
+        scanf("%d%d", &lhs, &rhs);
+
+        if (lhs - 5 < REGISTERS && rhs - 5 < REGISTERS) {
+          regs[lhs - 5] += regs[rhs - 5];
+          flag = regs[lhs - 5] == 0;
+        } else {
+          com = 0;
+          printf("\n%d", -1);
+          continue;
+        }
+        break;
+
+      case 2:
+        scanf("%d%d", &lhs, &rhs);
+
+        if (lhs - 5 < REGISTERS && rhs - 5 < REGISTERS) {
+          regs[lhs - 5] -= regs[rhs - 5];
+          flag = regs[lhs - 5] == 0;
+        } else {
+          com = 0;
+          printf("\n%d", -1);
+          continue;
+        }
+        break;
+
+      case 3:
+        scanf("%d%d", &lhs, &rhs);
+
+        if (lhs - 5 < REGISTERS) {
+          regs[lhs - 5] = rhs;
+          flag = regs[lhs - 5] == 0;
+        } else {
+          com = 0;
+          printf("\n%d", -1);
+          continue;
+        }
+        break;
+
+      case 4:
+        printf("\n");
+
+        for (size_t i = 0; i < REGISTERS; ++i) {
+          printf("%d ", regs[i]);
+        }
+        break;
+    }
+
+    scanf("\n%d", &com);
   }
-
-  thr_limit = thr_av / act_thr;
-  reg_limit = reg_av / (act_thr * act_reg);
-  mem_limit = mem_av / act_mem;
-
-  out = fopen("cuda.out", "w");
-  fprintf(out, "%d", (int) (((long double) thr * min(thr_limit, reg_limit, mem_limit)) / (long double) thr_av * 100 + 0.5));
-  fclose(out);
 }
