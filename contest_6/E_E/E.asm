6a7df7f114405a68862857e098bb7c3554a005c9
cd7567 <alexander.y.koshelev@gmail.com>
2022-11-11 16:40:25 +0300

    6_E_E added

---
+++ contest_6/E_E/E.asm
@@ -0,0 +1,39 @@
+global main
+extern scanf
+extern printf
+
+section .data
+    d_fmt_out: db "%d", 10, 0
+    d_fmt_in: db "%d", 0
+
+section .bss
+    lhs: resd 1
+    rhs: resd 1
+
+section .text
+main:
+    push rbp
+    mov rdi, d_fmt_in
+    mov rsi, dword lhs
+    call scanf
+    pop rbp
+
+    push rbp
+    mov rdi, d_fmt_in
+    mov rsi, dword rhs
+    call scanf
+    pop rbp
+
+    mov eax, dword [lhs]
+    mov ebx, dword [rhs]
+    cdq
+    idiv ebx
+
+    push rbp
+    mov rdi, d_fmt_out
+    mov esi, edx
+    call printf
+    pop rbp
+
+    mov rax, 0
+    ret
